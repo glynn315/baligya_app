0e9d1b9e65ba1a0bed05f007393ee68dec502859
ygsorianochiukim <ygsoriano@mail.corpmail.info>
2026-06-18 21:43:20 +0800

    fix: update apiBaseUrl for production and development environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // apiBaseUrl: 'http://127.0.0.1:8000/api/v1',
-  apiBaseUrl: 'http://192.168.50.103:8000/api/v1',
+  apiBaseUrl: 'http://127.0.0.1:8001/api/v1',
+  // apiBaseUrl: 'http://192.168.50.103:8001/api/v1',
   appName: 'Baligya',
   storageKeys: {
     accessToken: 'baligya.access_token',
